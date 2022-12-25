1217bd5278b4ce918f5c46ddc680d266f26554b8
short_horizon451/caleb <115152760+shorthorizon451@users.noreply.github.com>
2022-12-25 16:03:57 +0800

    Add files via upload

---
+++ MerryXMAS-NwYr/mxmasanyrModule.vb
@@ -0,0 +1,49 @@
+Module mxmasanyrModule
+    Sub Main()
+start:
+        TextWindow.WriteLine("")
+        TextWindow.ForegroundColor = 12
+        Program.Delay(50)
+        TextWindow.WriteLine(" __          __             _     _                                   __  __                         _____ _          _     _                       ")
+        Program.Delay(50)
+        TextWindow.WriteLine(" \ \        / /            (_)   | |                                 |  \/  |                       / ____| |        (_)   | |                      ")
+        Program.Delay(50)
+        TextWindow.ForegroundColor = 15
+        TextWindow.WriteLine("  \ \  /\  / /__  __      ___ ___| |__    _   _  ___  _   _    __ _  | \  / | ___ _ __ _ __ _   _  | |    | |__  _ __ _ ___| |_ _ __ ___   __ _ ___ ")
+        Program.Delay(50)
+        TextWindow.WriteLine("   \ \/  \/ / _ \ \ \ /\ / / / __| '_ \  | | | |/ _ \| | | |  / _` | | |\/| |/ _ \ '__| '__| | | | | |    | '_ \| '__| / __| __| '_ ` _ \ / _` / __|")
+        Program.Delay(50)
+        TextWindow.ForegroundColor = 2
+        TextWindow.WriteLine("    \  /\  /  __/  \ V  V /| \__ \ | | | | |_| | (_) | |_| | | (_| | | |  | |  __/ |  | |  | |_| | | |____| | | | |  | \__ \ |_| | | | | | (_| \__ \")
+        Program.Delay(50)
+        TextWindow.WriteLine("     \/  \/ \___|   \_/\_/ |_|___/_| |_|  \__, |\___/ \__,_|  \__,_| |_|  |_|\___|_|  |_|   \__, |  \_____|_| |_|_|  |_|___/\__|_| |_| |_|\__,_|___/")
+        Program.Delay(50)
+        TextWindow.ForegroundColor = 9
+        TextWindow.WriteLine("                                           __/ |                                             __/ |                                                  ")
+        Program.Delay(50)
+        TextWindow.WriteLine("                     _           _    _   |___/                   _   _                __   |___/            _                                      ")
+        Program.Delay(50)
+        TextWindow.ForegroundColor = 10
+        TextWindow.WriteLine("     /\             | |         | |  | |                         | \ | |               \ \   / /            | |                                     ")
+        Program.Delay(50)
+        TextWindow.WriteLine("    /  \   _ __   __| |   __ _  | |__| | __ _ _ __  _ __  _   _  |  \| | _____      __  \ \_/ /__  __ _ _ __| |                                     ")
+        Program.Delay(50)
+        TextWindow.ForegroundColor = 14
+        TextWindow.WriteLine("   / /\ \ | '_ \ / _` |  / _` | |  __  |/ _` | '_ \| '_ \| | | | | . ` |/ _ \ \ /\ / /   \   / _ \/ _` | '__| |                                     ")
+        Program.Delay(50)
+        TextWindow.WriteLine("  / ____ \| | | | (_| | | (_| | | |  | | (_| | |_) | |_) | |_| | | |\  |  __/\ V  V /     | |  __/ (_| | |  |_|                                     ")
+        Program.Delay(50)
+        TextWindow.ForegroundColor = 11
+        TextWindow.WriteLine(" /_/    \_\_| |_|\__,_|  \__,_| |_|  |_|\__,_| .__/| .__/ \__, | |_| \_|\___| \_/\_/      |_|\___|\__,_|_|  (_)                                     ")
+        Program.Delay(50)
+        TextWindow.WriteLine("                                             | |   | |     __/ |                                                                                    ")
+        Program.Delay(50)
+        TextWindow.ForegroundColor = 7
+        TextWindow.WriteLine("                                             |_|   |_|    |___/                                                                                     ")
+        Program.Delay(50)
+        TextWindow.WriteLine("")
+        Program.Delay(120)
+        TextWindow.Clear()
+        Goto start
+    End Sub
+End Module
